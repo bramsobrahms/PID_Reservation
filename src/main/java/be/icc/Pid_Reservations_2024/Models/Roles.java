88f95c00ff5904d2d5d4481b5d8eaf868c79f269
Brahms <bramso.brahms@gmail.com>
2024-11-15 22:27:43 +0100

    Create Roles Model

---
+++ src/main/java/be/icc/Pid_Reservations_2024/Models/Roles.java
@@ -0,0 +1,35 @@
+package be.icc.Pid_Reservations_2024.Models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+@Table(name = "Roles")
+public class Roles {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "role", length = 30)
+    private String role;
+
+    // Constructor by default
+    public Roles() { }
+
+    //Constructor with params
+    public Roles(Long id, String role) {
+        this.id = id;
+        this.role = role;
+    }
+
+    // ToString for role
+    @Override
+    public String toString() {
+        return "Roles{" +
+                "role='" + role + '\'' +
+                '}';
+    }
+
+}
