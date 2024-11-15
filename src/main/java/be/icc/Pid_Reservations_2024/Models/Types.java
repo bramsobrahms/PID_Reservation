e583b2b0ae70eda3e9a7530eaa55ad553ab51d3b
Brahms <bramso.brahms@gmail.com>
2024-11-15 22:20:54 +0100

    Create Types Model

---
+++ src/main/java/be/icc/Pid_Reservations_2024/Models/Types.java
@@ -0,0 +1,44 @@
+package be.icc.Pid_Reservations_2024.Models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+@Table(name = "Types")
+public class Types {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "type", length = 60)
+    @NotBlank(message = "The type must not be empty")
+    @Size(max=60, message = "The type must be 60 characters")
+    private String type;
+
+    // Constructor by default
+    protected Types() {}
+
+    // Constructor with params
+    public Types(Long id, String type) {
+        this.id = id;
+        this.type = type;
+    }
+
+    // Constructor without id
+    public Types(String type) {
+        this.type = type;
+    }
+
+    // ToString
+    @Override
+    public String toString() {
+        return "Types{" +
+                "type='" + type + '\'' +
+                '}';
+    }
+
+}
