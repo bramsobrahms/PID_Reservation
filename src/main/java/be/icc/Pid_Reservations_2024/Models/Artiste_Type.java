9fcb2beca356a9a62694ac950a18f92c58540c35
Brahms <bramso.brahms@gmail.com>
2024-11-16 00:47:15 +0100

    Add Relation OneToMany

--- src/main/java/be/icc/Pid_Reservations_2024/Models/Artiste_Type.java
+++ src/main/java/be/icc/Pid_Reservations_2024/Models/Artiste_Type.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Set;
+
 @Entity
 @Getter @Setter
 @Table( name = "Artiste_Type")
@@ -12,6 +14,10 @@ public class Artiste_Type {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    // Relation One To Many
+    @OneToMany(mappedBy = "artist_type")
+    private Set<Artiste_Type_Show> artiste_type_show;
 
     // Constructor by default
     protected Artiste_Type() {}
