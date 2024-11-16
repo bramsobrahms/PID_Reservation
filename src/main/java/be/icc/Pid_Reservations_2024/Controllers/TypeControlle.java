6f6ef10f16df637544489e74ce7f9ee1a0788903
Brahms <bramso.brahms@gmail.com>
2024-11-16 18:25:40 +0100

    Add injection dependence @Controller

--- src/main/java/be/icc/Pid_Reservations_2024/Controllers/TypeControlle.java
+++ src/main/java/be/icc/Pid_Reservations_2024/Controllers/TypeControlle.java
@@ -3,11 +3,13 @@ package be.icc.Pid_Reservations_2024.Controllers;
 import be.icc.Pid_Reservations_2024.Models.Types;
 import be.icc.Pid_Reservations_2024.Services.TypeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
+@Controller
 public class TypeControlle {
 
     @Autowired
