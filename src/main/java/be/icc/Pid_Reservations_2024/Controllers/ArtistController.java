afc5938eac923c436a1b7dfffc5518a2b405b8d7
Brahms <bramso.brahms@gmail.com>
2024-11-16 17:33:53 +0100

    Edit Artist on controller

--- src/main/java/be/icc/Pid_Reservations_2024/Controllers/ArtistController.java
+++ src/main/java/be/icc/Pid_Reservations_2024/Controllers/ArtistController.java
@@ -2,15 +2,13 @@ package be.icc.Pid_Reservations_2024.Controllers;
 
 import be.icc.Pid_Reservations_2024.Models.Artists;
 import be.icc.Pid_Reservations_2024.Services.ArtistService;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -56,6 +54,39 @@ public class ArtistController {
         }
 
         artistService.addArtist(artist);
+
+        return "redirect:/artist/"+artist.getId();
+    }
+
+    @GetMapping("/artist/{id}/edit")
+    public String edit(Model model, @PathVariable("id") long id, HttpServletRequest httpServletRequest) {
+        Artists artist = artistService.getArtist(id);
+
+        model.addAttribute("artist",artist);
+
+        // Generate the return link for cancel
+        String referrer = httpServletRequest.getHeader("Referer");
+
+        if(referrer != null && !referrer.equals("")) {
+            model.addAttribute("back", referrer);
+        }else {
+            model.addAttribute("back", "/artist/"+artist.getId());
+        }
+
+        return "Artist/edit";
+    }
+    @PutMapping("/artist/{id}/edit")
+    public String update(@Valid @ModelAttribute("artist") Artists artist, BindingResult bindingResult, @PathVariable("id") long id,Model model) {
+        if(bindingResult.hasErrors()) {
+            return "Artist/edit";
+        }
+
+        Artists artistExisting = artistService.getArtist(id);
+
+        if(artistExisting == null) {
+            return "Artist/index";
+        }
+        artistService.updateArtist(id, artist);
 
         return "redirect:/artist/"+artist.getId();
     }
