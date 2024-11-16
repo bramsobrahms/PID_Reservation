a420d8b254fb4828e851de4da91afa12baff6c4e
Brahms <bramso.brahms@gmail.com>
2024-11-16 18:50:31 +0100

    Find By id type

--- src/main/java/be/icc/Pid_Reservations_2024/Controllers/TypeControlle.java
+++ src/main/java/be/icc/Pid_Reservations_2024/Controllers/TypeControlle.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -23,6 +24,16 @@ public class TypeControlle {
         model.addAttribute("title", "List of types");
 
         return "Type/index";
+    }
+
+    @GetMapping("type/{id}")
+    public String show(@PathVariable("id") long id, Model model) {
+        Types type = typeService.getType(id);
+
+        model.addAttribute("type", type);
+        model.addAttribute("title", "Type Details");
+
+        return "type/show";
     }
 
 }
