008587e2d21a776c0668fc4408505ced243e0ab4
Brahms <bramso.brahms@gmail.com>
2024-11-16 14:42:37 +0100

    HomeController with HTML

---
+++ src/main/java/be/icc/Pid_Reservations_2024/Controllers/HelloController.java
@@ -0,0 +1,14 @@
+package be.icc.Pid_Reservations_2024.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Home/index";
+    }
+
+}
