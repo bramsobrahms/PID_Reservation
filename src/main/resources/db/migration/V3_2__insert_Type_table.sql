156e59ad4fb9929689527e99adff13f69f807f75
Brahms <bramso.brahms@gmail.com>
2024-11-19 16:53:02 +0100

    Flyway V3_3__insert_Type_tables.sql

--- src/main/resources/db/migration/V3_2__insert_Type_table.sql
+++ src/main/resources/db/migration/V3_2__insert_Type_table.sql
@@ -1,6 +1,4 @@
 INSERT INTO `types`(`id`, `type`)
-VALUES (1, 'Compositeur'),
-       (2,'Musicien'),
-       (3,'Directeur artistique'),
-       (4,'Cinéaste'),
-       (5,'Interprète');
+VALUES (1, 'Auteur'),
+       (2,'Scénographe'),
+       (3,'Comédien');
