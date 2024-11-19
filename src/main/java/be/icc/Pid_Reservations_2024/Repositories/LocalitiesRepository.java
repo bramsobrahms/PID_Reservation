caa51ffe41b8e025891c08502019a965c244ce2c
Brahms <bramso.brahms@gmail.com>
2024-11-19 16:42:38 +0100

    Add method get Locality and postalCode Service

--- src/main/java/be/icc/Pid_Reservations_2024/Repositories/LocalitiesRepository.java
+++ src/main/java/be/icc/Pid_Reservations_2024/Repositories/LocalitiesRepository.java
@@ -3,10 +3,10 @@ package be.icc.Pid_Reservations_2024.Repositories;
 import be.icc.Pid_Reservations_2024.Models.Localities;
 import org.springframework.data.repository.CrudRepository;
 
-public interface LocalityRepository extends CrudRepository<Localities, Long> {
+public interface LocalitiesRepository extends CrudRepository<Localities, Long> {
 
-    Localities finByPostalCode(String postalCode);
+    Localities findByPostalCode(String postalCode);
 
-    Localities findbyLocality(String locality);
+    Localities findByLocality(String locality);
 
 }
