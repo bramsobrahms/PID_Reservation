7bd0d153eba958d5ebd210c8333522a659569c84
Brahms <bramso.brahms@gmail.com>
2024-11-19 14:44:40 +0100

    Add fidByID an findByType

--- src/main/java/be/icc/Pid_Reservations_2024/Repositories/TypeRepository.java
+++ src/main/java/be/icc/Pid_Reservations_2024/Repositories/TypeRepository.java
@@ -3,9 +3,13 @@ package be.icc.Pid_Reservations_2024.Repositories;
 import be.icc.Pid_Reservations_2024.Models.Types;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface TypeRepository extends CrudRepository<Types, Long> {
 
-    Types findById(long id);
+    Optional<Types> findById(long id);
+
+    Types findByType(String type);
 
     Types save(Types type);
 
