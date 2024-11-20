1f8c5869d754cb2b4481404a7d42f1356d89e195
Brahms <bramso.brahms@gmail.com>
2024-11-20 21:49:36 +0100

    Show locality by id

--- src/main/java/be/icc/Pid_Reservations_2024/Controllers/LocalityController.java
+++ src/main/java/be/icc/Pid_Reservations_2024/Controllers/LocalityController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -19,7 +20,16 @@ public class LocalityController {
     public String index(Model model) {
         List<Localities> localities = localitiesService.getAll();
         model.addAttribute("localities", localities);
-        model.addAttribute("title", "list of localities");
+        model.addAttribute("title", "List of localities");
         return "Locality/index";
+    }
+
+    @GetMapping("/locality/{id}")
+    public String show(@PathVariable long id, Model model) {
+        Localities locality = localitiesService.getById(id);
+        model.addAttribute("locality", locality);
+        model.addAttribute("title", "Locality profile");
+
+        return "Locality/show";
     }
 }
