3984f35443e698c4cb2986bde1e13fb5ea9fdb27
Brahms <bramso.brahms@gmail.com>
2025-01-16 19:56:45 +0100

    insert some datas inside database

---
+++ src/main/resources/db/migration/V2_12__insert_reviews_table.sql
@@ -0,0 +1,12 @@
+INSERT INTO `reviews` (`show_id`, `user_id`, `stars`, `is_validated`, `review`, `create_at`)
+VALUES
+    (1, 1, 5, 1, 'Un concert magique, Sting a su captiver le public !', '2025-01-15 12:00:00'),
+    (2, 2, 4, 1, 'Emma Watson est incroyable dans ce rôle, mais la mise en scène pouvait être meilleure.', '2025-01-12 15:30:00'),
+    (3, 3, 5, 1, 'Danse contemporaine fascinante, Sasha Waltz a livré une performance impressionnante.', '2025-01-10 18:20:00'),
+    (4, 4, 5, 1, 'Les illusions de David Copperfield sont à couper le souffle, un spectacle à ne pas manquer !', '2025-01-09 14:00:00'),
+    (5, 5, 4, 1, 'Ariana Grande a livré un concert énergique, mais la setlist était un peu courte.', '2025-01-08 16:45:00'),
+    (6, 6, 5, 1, 'Une expérience cinématographique unique, Tarantino est un génie !', '2025-01-07 18:30:00'),
+    (7, 7, 5, 1, "L'exposition Banksy à Paris est une véritable immersion dans son art.", '2025-01-06 17:10:00'),
+    (8, 8, 5, 1, 'Un ballet magique, Misty Copeland est une reine du dance classique.', '2025-01-05 20:00:00'),
+    (9, 9, 4, 1, 'Un concert incroyable, mais un peu trop long pour moi.', '2025-01-04 19:30:00'),
+    (10, 10, 5, 1, "Gad Elmaleh m'a fait pleurer de rire, un spectacle à ne pas manquer !", '2025-01-03 21:00:00');
---
+++ src/main/resources/db/migration/V2_2__insert_types_table.sql
@@ -0,0 +1,12 @@
+INSERT INTO `types` (`type`)
+VALUES
+    ('Musicien'),
+    ('Comédien'),
+    ('Chorégraphe'),
+    ('Magicien'),
+    ('Chanteur'),
+    ('Cinéaste'),
+    ('Artiste visuel'),
+    ('Danseur'),
+    ('Compositrice'),
+    ('Humoriste');
