8c2339645f0324f110d70e32d270917324cf34cd
Brahms <bramso.brahms@gmail.com>
2025-03-05 18:30:13 +0100

    Change name picture

--- src/main/resources/db/migration/V2_8__insert_shows_table.sql
+++ src/main/resources/db/migration/V2_8__insert_shows_table.sql
@@ -1,12 +1,12 @@
 INSERT INTO `shows` (`location_id`, `is_bookable`, `duration`, `slug`, `title`, `poster_url`)
 VALUES
-    (1, 1, 120, 'sting-concert-paris', 'Sting en Concert à Paris', 'https://www.example.com/stingconcert.jpg'),
-    (2, 1, 150, 'emma-watson-play-paris', 'Emma Watson dans “La Belle et la Bête”', 'https://www.example.com/emmawatsonplay.jpg'),
-    (3, 1, 180, 'sasha-waltz-dance-lyon', 'Sasha Waltz - Danse Contemporaine', 'https://www.example.com/sashawaltz.jpg'),
-    (4, 1, 130, 'david-copperfield-magic-bordeaux', 'David Copperfield - Magie Illusionniste', 'https://www.example.com/copperfieldmagic.jpg'),
-    (5, 1, 90, 'ariana-grande-concert-nice', 'Ariana Grande en Concert à Nice', 'https://www.example.com/arianagrande.jpg'),
-    (6, 1, 120, 'quentin-tarantino-cinema-paris', 'Quentin Tarantino présente ses Films à Paris', 'https://www.example.com/tarantino.jpg'),
-    (7, 1, 100, 'banksy-exposition-paris', 'Banksy – Art Contemporain', 'https://www.example.com/banksy.jpg'),
-    (8, 1, 150, 'misty-copeland-ballet-marseille', 'Misty Copeland – Ballet Classique', 'https://www.example.com/misty.jpg'),
-    (9, 1, 120, 'lorde-concert-toulouse', 'Lorde en Concert à Toulouse', 'https://www.example.com/lorde.jpg'),
-    (10, 1, 120, 'gad-elmaleh-comedie-nantes', 'Gad Elmaleh – Humoriste à Nantes', 'https://www.example.com/gadelmaleh.jpg');
+    (1, 1, 120, 'sting-concert-paris', 'Sting en Concert à Paris', 'stingaparis.jpg'),
+    (2, 1, 150, 'emma-watson-play-paris', 'Emma Watson dans “La Belle et la Bête”', 'emmawatsonsdanslabelleetlabete.jpg'),
+    (3, 1, 180, 'sasha-waltz-dance-lyon', 'Sasha Waltz - Danse Contemporaine', 'sashawaltz.jpg'),
+    (4, 1, 130, 'david-copperfield-magic-bordeaux', 'David Copperfield - Magie Illusionniste', 'davidcopperfield.jpg'),
+    (5, 1, 90, 'ariana-grande-concert-nice', 'Ariana Grande en Concert à Nice', 'arianagrande.jpg'),
+    (6, 1, 120, 'quentin-tarantino-cinema-paris', 'Quentin Tarantino présente ses Films à Paris', 'quentintarantino.jpg'),
+    (7, 1, 100, 'banksy-exposition-paris', 'Banksy – Art Contemporain', 'banksy.jpg'),
+    (8, 1, 150, 'misty-copeland-ballet-marseille', 'Misty Copeland – Ballet Classique', 'mistycopeland.jpg'),
+    (9, 1, 120, 'lorde-concert-toulouse', 'Lorde en Concert à Toulouse', 'lorde.jpg'),
+    (10, 1, 120, 'gad-elmaleh-comedie-nantes', 'Gad Elmaleh – Humoriste à Nantes', 'gadelmaleh.jpg');
