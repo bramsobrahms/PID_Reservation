b17817ea3efb5b2d78b8ab5814dbd7f0efc40ad5
Brahms <bramso.brahms@gmail.com>
2025-04-12 17:26:38 +0200

    Remove represenation with s and update other class

--- src/main/java/be/icc/Pid_Reservations_2024/Repositories/RepresentationRepository.java
+++ src/main/java/be/icc/Pid_Reservations_2024/Repositories/RepresentationRepository.java
@@ -1,11 +1,7 @@
 package be.icc.Pid_Reservations_2024.Repositories;
 
-
 import be.icc.Pid_Reservations_2024.Models.Representation;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
 public interface RepresentationRepository extends JpaRepository<Representation, Long> {
-
 }
