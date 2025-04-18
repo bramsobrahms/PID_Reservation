f3ab2322810bab56b859e44a23e450e1a70e6f8f
Brahms <bramso.brahms@gmail.com>
2025-04-18 23:27:10 +0200

    Create ShowApiController CRUD

--- src/main/java/be/icc/Pid_Reservations_2024/api/Controllers/ShowApiController.java
+++ src/main/java/be/icc/Pid_Reservations_2024/api/Controllers/ShowApiController.java
@@ -8,9 +8,13 @@ import be.icc.Pid_Reservations_2024.api.Dto.ShowsDto;
 import be.icc.Pid_Reservations_2024.api.Hateoas.showModelAssembler;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping("/api")
@@ -18,68 +22,68 @@ public class ShowApiController {
 
     private final ShowRepository showRepository;
     private final showModelAssembler showAssembler;
-    private final SpringSecurityConfig springSecurityConfig;
 
-    public ShowApiController(ShowRepository showRepository, showModelAssembler showAssembler, SpringSecurityConfig springSecurityConfig) {
+    public ShowApiController(ShowRepository showRepository, showModelAssembler showAssembler) {
         this.showRepository = showRepository;
         this.showAssembler = showAssembler;
-        this.springSecurityConfig = springSecurityConfig;
     }
+
 
     @GetMapping("/shows")
-    public CollectionModel<EntityModel<ShowsDto>> allShows() {
-        List<EntityModel<ShowsDto>> shows = showRepository.findAll()
-                .stream()
-                .map(this::showsToDto)
-                .map(showAssembler::toModel)
-                .toList();
+    public CollectionModel<EntityModel<Show>> allShows() {
+        List<EntityModel<Show>> shows = showRepository.findAll() // Get all artists from DB
+                .stream() // Convert the list of artists into a stream to apply functional operations
+                .map(showAssembler::toModel) // Convert each `Artist` object into an `EntityModel<Artist> added HATEOAS links`
+                .toList(); // Collect the results into a list
 
-        return CollectionModel.of(shows);
+        // Create a Collection with the list of artists and add a HATEOAS link to the "/artists" endpoint
+        return CollectionModel.of(shows, // The list of 'EntityModel<Artist>' objects
+                linkTo(methodOn(ShowApiController.class).allShows()) // Create a link to the 'allArtists()' method in the same controller
+                        .withRel("All shows"));  // Add a 'self' link to this resource (refers to itself)
     }
 
-    private ShowsDto showsToDto(Show show) {
-        ShowsDto showsDto = new ShowsDto();
-        showsDto.setId(show.getId());
-        showsDto.setTitle(show.getTitle());
-        showsDto.setPosterUrl(show.getPosterUrl());
-        return showsDto;
-    }
-
-    @GetMapping("/show/{id}")
-    public EntityModel<ShowIdDto> showById(@PathVariable Long id) {
-        Show showId = showRepository.findById(id).orElse(null);
-
-        ShowIdDto showIdDto = showIdToDto(showId);
-
-        return showAssembler.toModel(showIdDto);
-    }
-
-    private ShowIdDto showIdToDto(Show show) {
-        ShowIdDto dto = new ShowIdDto();
-        dto.setId(show.getId());
-        dto.setTitle(show.getTitle());
-        dto.setPosterUrl(show.getPosterUrl());
-        dto.setDuration(show.getDuration());
-        dto.setCreated_in(show.getCreated_in());
-        dto.setIsBookable(show.getIsBookable());
-        return dto;
-    }
-
-    @PutMapping("/show/edit/{id}")
-    public EntityModel<Show> updateShow(@PathVariable Long id, @RequestBody Show showUpdate) {
+    @GetMapping("/shows/{id}")
+    public EntityModel<Show> aShow(@PathVariable long id) {
         Show show = showRepository.findById(id).orElse(null);
-
-        if (show != null) {
-            show.setTitle(showUpdate.getTitle());
-            show.setPosterUrl(showUpdate.getPosterUrl());
-            show.setDuration(showUpdate.getDuration());
-            show.setCreated_in(showUpdate.getCreated_in());
-            show.setIsBookable(showUpdate.getIsBookable());
-            showRepository.save(show);
-        }
-
-
         return showAssembler.toModel(show);
+    }
+
+    @PostMapping("/admin/show")
+    public ResponseEntity<?> newShow(@RequestBody Show newShow) {
+        Show saveNewShow = showRepository.save(newShow);
+        return ResponseEntity.created(linkTo(methodOn(ShowApiController.class)
+                        .aShow(saveNewShow.getId())) // Pass the ID of the saved artist
+                        .toUri())  // Convert the link into a URI format
+                .body(saveNewShow); // Include the saved artist object in the response body
+    }
+
+    @PutMapping("/admin/show/{id}")
+    public ResponseEntity<?> updateShow(@RequestBody Show updatedShow, @PathVariable long id) {
+        Show updateTheShow = showRepository.findById(id)
+                .map(show -> { // If the artist exists, update their information
+                    show.setTitle(updatedShow.getTitle());
+                    show.setPosterUrl(updatedShow.getPosterUrl());
+                    show.setDuration(updatedShow.getDuration());
+                    show.setCreated_in(updatedShow.getCreated_in());
+                    show.setIsBookable(updatedShow.getIsBookable());
+                    return showRepository.save(show);
+                }).orElseGet(() -> { // If the artist doesn't exist, create a new one
+                    updatedShow.setId(id); // Set the ID for the new artist
+                    return showRepository.save(updatedShow);
+                });
+
+        return ResponseEntity.created( // Indicates that the resource was created successfully
+                        linkTo(methodOn(ShowApiController.class) // Creates a link to the "anArtist" method for the updated artist
+                                .aShow(updateTheShow.getId())) // Pass the ID of the updated artis
+                                .toUri()) // Convert the link into a URI format
+                .body(showAssembler.toModel(updateTheShow)); // Return the updated artist as a HATEOAS model
+    }
+
+    @DeleteMapping("/admin/show/{id}")
+    public ResponseEntity<?> deleteShow(@PathVariable long id) {
+        showRepository.deleteById(id);
+        // Return a ResponseEntity with a "no content" status (204), indicating the deletion was successful
+        return ResponseEntity.noContent().build();
     }
 
 
