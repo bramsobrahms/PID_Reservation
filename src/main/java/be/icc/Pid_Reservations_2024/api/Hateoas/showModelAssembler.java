1e8e8e65ccbefecdba446583b4624fee337e6aee
Brahms <bramso.brahms@gmail.com>
2025-04-19 00:22:59 +0200

    a good JavaDoc for Hateoas

--- src/main/java/be/icc/Pid_Reservations_2024/api/Hateoas/showModelAssembler.java
+++ src/main/java/be/icc/Pid_Reservations_2024/api/Hateoas/showModelAssembler.java
@@ -2,8 +2,6 @@ package be.icc.Pid_Reservations_2024.api.Hateoas;
 
 import be.icc.Pid_Reservations_2024.Models.Show;
 import be.icc.Pid_Reservations_2024.api.Controllers.ShowApiController;
-import be.icc.Pid_Reservations_2024.api.Dto.ShowIdDto;
-import be.icc.Pid_Reservations_2024.api.Dto.ShowsDto;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
 import org.springframework.stereotype.Component;
@@ -11,23 +9,29 @@ import org.springframework.stereotype.Component;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
+/**
+ * Converts a `Show` object into an `EntityModel<Show>` and adds HATEOAS links to it.
+ * HATEOAS (Hypermedia As The Engine Of Application State) is used to provide links
+ * that can guide the client through available actions in the API.
+ */
 @Component
 public class showModelAssembler implements RepresentationModelAssembler<Show, EntityModel<Show>> {
     /**
      * Converts an `Show` object into an `EntityModel<Artist>`, adding HATEOAS links.
      * HATEOAS => Hypermedia As The Engine Of Application State
      *
-     * @param show The artist to convert into an EntityModel.
-     * @return An `EntityModel<Artist>` with the artist a links for navigation in the API
+     * @param show The Show object to convert.
+     * @return An `EntityModel<Show>` containing the Show object and links to related actions.
      */
     @Override
     public EntityModel<Show> toModel(Show show) {
+        // Create and return an EntityModel containing the show and relevant HATEOAS links
         return EntityModel.of(show,
-                linkTo(methodOn(ShowApiController.class).allShows()).withRel("All shows"),
-                linkTo(methodOn(ShowApiController.class).aShow(show.getId())).withSelfRel(),
-                linkTo(methodOn(ShowApiController.class).newShow(show)).withRel("Create new Show"),
-                linkTo(methodOn(ShowApiController.class).updateShow(show, show.getId())).withRel("Update show"),
-                linkTo(methodOn(ShowApiController.class).deleteShow(show.getId())).withRel("Delete")
+                linkTo(methodOn(ShowApiController.class).allShows()).withRel("All shows"),// Link to get a list of all shows
+                linkTo(methodOn(ShowApiController.class).aShow(show.getId())).withSelfRel(),// Self link to get the specific show by its ID
+                linkTo(methodOn(ShowApiController.class).newShow(show)).withRel("Create new Show"),// Link to create a new show
+                linkTo(methodOn(ShowApiController.class).updateShow(show, show.getId())).withRel("Update show"),// Link to update the specific show by its ID
+                linkTo(methodOn(ShowApiController.class).deleteShow(show.getId())).withRel("Delete")// Link to delete the show by its ID
         );
 
     }
